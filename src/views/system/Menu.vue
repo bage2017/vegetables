2c0f1d0877da7ed814c573002b2efe328802b526
bage2017 <hxiaoxuey@163.com>
2017-12-28 23:49:07 +0800

    12.28 23：49

--- src/views/system/Menu.vue
+++ src/views/system/Menu.vue
@@ -19,7 +19,8 @@
             <template slot="title">
               <Icon :type="menu.MenuIcon"></Icon>{{menu.MenuName}}
             </template>
-            <Menu-item :name="item.ID" v-for="item in menu.Children" :key="item.ID">{{item.MenuName}}</Menu-item>
+            <Menu-item :name="item.ID" v-for="item in menu.Children" :key="item.ID">
+              <Icon :type="item.MenuIcon"></Icon>{{item.MenuName}}</Menu-item>
           </Submenu>
         </Menu>
         </Col>
@@ -57,210 +58,214 @@
 </template>
 <script>
 export default {
-    name: "menu",
-    data() {
-        return {
-            menus: [],
-            previewmenus: [],
-            parents: [],
-            save_loading: false,
-            actiontype: "MenuAdd",
-            formInfo: {
-                ID: 0,
-                Pid: 0,
-                MenuName: "",
-                MenuHref: "",
-                MenuIcon: "",
-                MenuOrder: 1
-            },
-            selectNode: {
-                ID: 0,
-                Pid: 0,
-                MenuName: "",
-                MenuHref: "",
-                MenuIcon: "",
-                MenuOrder: 1
-            },
-            ruleValidate: {
-                Pid: [
-                    {
-                        type: "number",
-                        required: true,
-                        message: "请选择父级菜单",
-                        trigger: "change"
-                    }
-                ],
-                MenuName: [
-                    { required: true, message: "菜单名称不能为空", trigger: "blur" },
-                    {
-                        type: "string",
-                        max: 20,
-                        message: "菜单名称最多20个字",
-                        trigger: "blur"
-                    }
-                ],
-                MenuHref: [
-                    { required: true, message: "菜单链接不能为空", trigger: "blur" },
-                    {
-                        type: "string",
-                        max: 200,
-                        message: "菜单链接最多200个字",
-                        trigger: "blur"
-                    }
-                ],
-                MenuIcon: [
-                    {
-                        type: "string",
-                        max: 50,
-                        message: "菜单图标最多50个字",
-                        trigger: "blur"
-                    }
-                ]
-            }
-        };
+  name: 'menu',
+  data() {
+    return {
+      menus: [],
+      previewmenus: [],
+      parents: [],
+      save_loading: false,
+      actiontype: 'MenuAdd',
+      formInfo: {
+        ID: 0,
+        Pid: 0,
+        MenuName: '',
+        MenuHref: '',
+        MenuIcon: '',
+        MenuOrder: 1
+      },
+      selectNode: {
+        ID: 0,
+        Pid: 0,
+        MenuName: '',
+        MenuHref: '',
+        MenuIcon: '',
+        MenuOrder: 1
+      },
+      ruleValidate: {
+        Pid: [
+          {
+            type: 'number',
+            required: true,
+            message: '请选择父级菜单',
+            trigger: 'change'
+          }
+        ],
+        MenuName: [
+          { required: true, message: '菜单名称不能为空', trigger: 'blur' },
+          {
+            type: 'string',
+            max: 20,
+            message: '菜单名称最多20个字',
+            trigger: 'blur'
+          }
+        ],
+        MenuHref: [
+          { required: true, message: '菜单链接不能为空', trigger: 'blur' },
+          {
+            type: 'string',
+            max: 200,
+            message: '菜单链接最多200个字',
+            trigger: 'blur'
+          }
+        ],
+        MenuIcon: [
+          {
+            type: 'string',
+            max: 50,
+            message: '菜单图标最多50个字',
+            trigger: 'blur'
+          }
+        ]
+      }
+    };
+  },
+  methods: {
+    getTree(tree = []) {
+      const arr = [];
+      if (tree && tree.length > 0) {
+        tree.forEach(item => {
+          const obj = {};
+          obj.title = item.MenuName;
+          obj.ID = item.ID; // 其他你想要添加的属性
+          obj.Pid = item.Pid;
+          obj.MenuHref = item.MenuHref;
+          obj.MenuIcon = item.MenuIcon;
+          obj.MenuOrder = item.MenuOrder;
+          obj.expand = false;
+          if (item.Children && item.Children.length > 0) {
+            obj.children = this.getTree(item.Children);
+          }
+          arr.push(obj);
+        });
+      }
+      return arr;
     },
-    methods: {
-        getTree(tree = []) {
-            const arr = [];
-            if (tree && tree.length > 0) {
-                tree.forEach(item => {
-                    const obj = {};
-                    obj.title = item.MenuName;
-                    obj.ID = item.ID; // 其他你想要添加的属性
-                    obj.Pid = item.Pid;
-                    obj.MenuHref = item.MenuHref;
-                    obj.MenuIcon = item.MenuIcon;
-                    obj.MenuOrder = item.MenuOrder;
-                    obj.expand = false;
-                    if (item.Children && item.Children.length > 0) {
-                        obj.children = this.getTree(item.Children);
-                    }
-                    arr.push(obj);
-                });
-            }
-            return arr;
-        },
-        mockTreeData() {
-            const vue = this;
-            vue.$store.dispatch("MenusTree", 0).then(result => {
-                if (result.Code == 200) {
-                    vue.menus = vue.getTree(result.Data);
-                    vue.previewmenus = result.Data[0].Children;
-                    vue.parents = [];
-                    vue.parents.push({
-                        Pid: result.Data[0].ID,
-                        MenuName: result.Data[0].MenuName
-                    });
-                    if (
-                        result.Data[0].Children &&
-                        result.Data[0].Children.length > 0
-                    ) {
-                        result.Data[0].Children.forEach(item => {
-                            vue.parents.push({
-                                Pid: item.ID,
-                                MenuName: item.MenuName
-                            });
-                        });
-                    }
-                } else {
-                    vue.$Message.error(result.Message);
-                }
+    mockTreeData() {
+      const vue = this;
+      vue.$store.dispatch('MenusTree', 0).then(result => {
+        if (result.Code == 200) {
+          vue.menus = vue.getTree(result.Data);
+          vue.previewmenus = result.Data[0].Children;
+          vue.parents = [];
+          vue.parents.push({
+            Pid: result.Data[0].ID,
+            MenuName: result.Data[0].MenuName
+          });
+          if (result.Data[0].Children && result.Data[0].Children.length > 0) {
+            result.Data[0].Children.forEach(item => {
+              vue.parents.push({
+                Pid: item.ID,
+                MenuName: item.MenuName
+              });
             });
-        },
-        adddata() {
-            this.actiontype = "MenuAdd";
-            this.$refs["formInfo"].resetFields();
-        },
-        deletedata() {
-            const vue = this;
-            if (vue.selectNode) {
-                vue.$Message.error("请选择节点");
-                return;
-            }
-            if (vue.selectNode.ID == 1) {
-                vue.$Message.error("顶级菜单不能删除");
-                return;
-            }
-            if (vue.selectNode.ID > 1) {
-                vue.$Modal.confirm({
-                    content: "<p>删除节点会删除所有子类.</p><p>确认删除?</p>",
-                    onOk: () => {
-                        vue.$store
-                            .dispatch("MenuDelete", snid)
-                            .then(result => {
-                                if (result.Code == 200) {
-                                    vue.$Message.success("删除成功");
-                                    vue.mockTreeData();
-                                } else {
-                                    vue.$Message.error(result.Message);
-                                }
-                            })
-                            .catch(err => {
-                                vue.$Message.error(err);
-                            });
-                    }
-                });
-            }
-        },
-        handleSubmit(name) {
-            if (this.formInfo.ID == 1) {
-                this.$Message.error("顶级菜单不能编辑");
-                return;
-            }
-            this.$refs[name].validate(valid => {
-                if (valid) {
-                    this.save_loading = true;
-                    if (this.formInfo.ID) {
-                        this.actiontype = "MenuModify";
-                    }
-                    this.$store
-                        .dispatch(this.actiontype, this.formInfo)
-                        .then(result => {
-                            if (result.Code == 200) {
-                                this.$Message.success("保存成功");
-                            } else {
-                                this.$Message.error(result.Message);
-                            }
-                        })
-                        .catch(err => {
-                            this.$Message.error(err);
-                        });
-                    setTimeout(() => {
-                        this.save_loading = false;
-                        this.$refs.formInfo.resetFields();
-                        this.mockTreeData();
-                    }, 2000);
-                } else {
-                    this.$Message.error("请输入完整信息!");
-                }
-                this.save_loading = false;
-            });
-        },
-        handleSelect(value) {
-            if (value && value[0]) {
-                if (value[0].ID > 1) {
-                    const sn = value[0];
-                    this.selectNode = {
-                        ID: sn.ID,
-                        Pid: sn.Pid,
-                        MenuName: sn.title,
-                        MenuHref: sn.MenuHref,
-                        MenuIcon: sn.MenuIcon,
-                        MenuOrder: sn.MenuOrder
-                    };
-                    this.formInfo = this.selectNode;
-                    this.$Notice.open({
-                        title: "选择节点提醒",
-                        desc: "选择了节点:" + sn.title
-                    });
-                } else if (value[0].ID == 1) {
-                    this.$Message.error("顶级菜单不能编辑!");
-                }
-            }
+          }
+        } else {
+          vue.$Message.error(result.Message);
         }
+      });
     },
-    created() {
-        this.mockTreeData();
+    adddata() {
+      this.actiontype = 'MenuAdd';
+      this.$refs.formInfo.resetFields();
+    },
+    deletedata() {
+      const vue = this;
+      if (!vue.selectNode || vue.selectNode.ID < 1) {
+        vue.$Message.error('请选择节点');
+      } else if (vue.selectNode.ID == 1) {
+        vue.$Message.error('顶级菜单不能删除');
+      } else {
+        vue.$Modal.confirm({
+          content: '<p>删除节点会删除所有子类.</p><p>确认删除?</p>',
+          onOk: () => {
+            vue.$store
+              .dispatch('MenuDelete', vue.selectNode.ID)
+              .then(result => {
+                if (result.Code == 200) {
+                  vue.$Message.success('删除成功');
+                  vue.mockTreeData();
+                } else {
+                  vue.$Message.error(result.Message);
+                }
+              })
+              .catch(err => {
+                vue.$Message.error(err);
+              });
+          }
+        });
+      }
+    },
+    handleSubmit(name) {
+      if (this.formInfo.ID == 1) {
+        this.$Message.error('顶级菜单不能编辑');
+      } else {
+        this.$refs[name].validate(valid => {
+          if (valid) {
+            this.save_loading = true;
+            if (this.formInfo.ID) {
+              this.actiontype = 'MenuModify';
+            }
+            this.$store
+              .dispatch(this.actiontype, this.formInfo)
+              .then(result => {
+                if (result.Code == 200) {
+                  this.$Message.success('保存成功');
+                } else {
+                  this.$Message.error(result.Message);
+                }
+              })
+              .catch(err => {
+                this.$Message.error(err);
+              });
+            setTimeout(() => {
+              this.save_loading = false;
+              this.$refs.formInfo.resetFields();
+              this.mockTreeData();
+            }, 2000);
+          } else {
+            this.$Message.error('请输入完整信息!');
+          }
+          this.save_loading = false;
+        });
+      }
+    },
+    handleSelect(value) {
+      const obj = {
+        ID: 0,
+        Pid: 0,
+        MenuName: '',
+        MenuHref: '',
+        MenuIcon: '',
+        MenuOrder: 1
+      };
+      if (value && value[0]) {
+        if (value[0].ID > 1) {
+          const sn = value[0];
+          this.selectNode = {
+            ID: sn.ID,
+            Pid: sn.Pid,
+            MenuName: sn.title,
+            MenuHref: sn.MenuHref,
+            MenuIcon: sn.MenuIcon,
+            MenuOrder: sn.MenuOrder
+          };
+          this.formInfo = this.selectNode;
+          this.$Notice.open({
+            title: '选择节点提醒',
+            desc: '选择了节点:' + sn.title
+          });
+        } else if (value[0].ID == 1) {
+          this.selectNode = obj;
+          this.$Message.error('顶级菜单不能编辑!');
+        }
+      } else {
+        this.selectNode = obj;
+      }
     }
+  },
+  created() {
+    this.mockTreeData();
+  }
 };
 </script>
---
+++ src/views/system/RoleMenu.vue
@@ -0,0 +1,127 @@
+<template>
+    <div class="animated fadeIn">
+        <Row>
+            <Col :md="12" >
+                    <div style="position:relative;">
+                        <Table highlight-row :columns="datacolumn" :data="datamodal" ref="table"></Table>
+                        <div style="position:absolute;top:0px;width:100%;height:100%;display: flex;
+                            align-items: center;
+                            justify-content: center;background: rgba(210, 216, 222, 0.5);" v-if="list_loadding">
+                            <Spin size="large"></Spin>
+                            <h6 style="color:#2d8cf0;margin-top:10px;">正在获取数据...</h6>
+                        </div>
+                    </div>
+            </Col>
+            <Col :md="12">
+            <Tree :data="data2" show-checkbox></Tree>
+            </Col>
+        </Row>
+    </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      datacolumn: [
+        {
+          title: '视频名称',
+          key: 'video_name',
+          ellipsis: 'true'
+        },
+        {
+          title: '上传时间',
+          ellipsis: 'true',
+          key: 'timestamp'
+        },
+        {
+          title: '大小',
+          ellipsis: 'true',
+          key: 'video_size',
+          filters: [
+            {
+              label: '大于10M',
+              value: 1
+            },
+            {
+              label: '小于10M',
+              value: 2
+            }
+          ],
+          filterMultiple: false,
+          filterMethod(value, row) {
+            if (value === 1) {
+              return row.video_size > 1024 * 1024 * 1024 * 10;
+            } else if (value === 2) {
+              return row.video_size < 1024 * 1024 * 10;
+            }
+          }
+        }
+      ],
+      data2: [
+        {
+          title: 'parent 1',
+          expand: true,
+          children: [
+            {
+              title: 'parent 1-1',
+              expand: true,
+              children: [
+                {
+                  title: 'leaf 1-1-1'
+                },
+                {
+                  title: 'leaf 1-1-2'
+                }
+              ]
+            },
+            {
+              title: 'parent 1-2',
+              expand: true,
+              children: [
+                {
+                  title: 'leaf 1-2-1'
+                },
+                {
+                  title: 'leaf 1-2-1'
+                }
+              ]
+            }
+          ]
+        }
+      ],
+      datamodal: [
+        {
+          timestamp: '2017-08-10 17:03:54',
+          video_size: 1732410,
+          video_name: 'p.mp4'
+        },
+        {
+          timestamp: '2017-08-10 17:03:54',
+          video_size: 1732410,
+          video_name: 'p.mp4'
+        },
+        {
+          timestamp: '2017-08-10 17:03:54',
+          video_size: 1732410,
+          video_name: 'p.mp4'
+        },
+        {
+          timestamp: '2017-08-10 17:03:54',
+          video_size: 1732410,
+          video_name: 'p.mp4'
+        },
+        {
+          timestamp: '2017-08-10 17:03:54',
+          video_size: 1732410,
+          video_name: 'p.mp4'
+        },
+        {
+          timestamp: '2017-08-10 17:03:54',
+          video_size: 1732410,
+          video_name: 'p.mp4'
+        }
+      ]
+    };
+  }
+};
+</script>
